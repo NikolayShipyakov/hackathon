7019e874671b062d9fda5949a45716a73d5374f7
NikolayShipyakov <Qwerty123>
2013-11-07 19:05:45 +0400

    Залил чисто чтобы самому поэксперементиролвать. Чтобы можно было и дома и на работе эксперементировать с одними прожектом

---
+++ ui/draw.js
@@ -0,0 +1,35 @@
+var stage = new Kinetic.Stage({
+    container: 'container',
+    width: 578,
+    height: 200
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+    x: 239,
+    y: 75,
+    width: 100,
+    height: 50,
+    fill: 'green',
+    stroke: 'black',
+    strokeWidth: 4
+});
+
+var circle = new Kinetic.Circle({
+    x: stage.getWidth() / 2,
+    y: stage.getHeight() / 2,
+    radius: 70,
+    fill: 'red'
+    //stroke: 'black',
+    //strokeWidth: 4
+});
+
+// add the shape to the layer
+layer.add(circle);
+
+// add the shape to the layer
+//layer.add(rect);
+
+// add the layer to the stage
+stage.add(layer);
