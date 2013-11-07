6ce919dc1242d49e82539cac7577a3400f2f9137
NikolayShipyakov <shipjakov@rambler.ru>
2013-11-08 01:21:01 +0400

    Нарисовал звезду

--- ui/draw.js
+++ ui/draw.js
@@ -1,32 +1,56 @@
+var stageWidth = screen.width * 0.95;
+var stageHeight = screen.height * 1;
+var bigRoundRad = stageHeight / 5;
+var littleRoundRad = stageHeight / 10;
+var gradDif = 360 / 5;
+var refFromCenter = stageHeight / 2.5;
+
 var stage = new Kinetic.Stage({
     container: 'container',
-    width: 578,
-    height: 200
+    width: stageWidth,
+    height: stageHeight
 });
 
 var layer = new Kinetic.Layer();
 
-var rect = new Kinetic.Rect({
-    x: 239,
-    y: 75,
-    width: 100,
-    height: 50,
-    fill: 'green',
-    stroke: 'black',
-    strokeWidth: 4
-});
-
-var circle = new Kinetic.Circle({
+var mainCircle = new Kinetic.Circle({
     x: stage.getWidth() / 2,
     y: stage.getHeight() / 2,
-    radius: 70,
+    radius: bigRoundRad,
     fill: 'red'
-    //stroke: 'black',
-    //strokeWidth: 4
 });
 
+function drawFriends(){
+    for (i = 0; i < 5; i++){
+        var rot = i * gradDif;
+        var multX;
+        var multY;
+        if((rot >=0 && rot <= 90) || (rot >= 270 && rot <= 360)){
+            multY = 1;
+        } else {
+            multY = -1;
+        }
+        if(rot >=0 && rot <= 180){
+            multX = 1;
+        } else {
+            multX = -1;
+        }
+        var xc = stage.getWidth() / 2 +  multX * refFromCenter * Math.abs(Math.sin(rot / 57.29));
+        var yc = stage.getHeight() / 2 - multY * refFromCenter * Math.abs(Math.cos(rot / 57.29));
+        var littleCircle = new Kinetic.Circle({
+            x: xc,
+            y: yc,
+            radius: littleRoundRad,
+            fill: 'green'
+        });
+        layer.add(littleCircle);
+    }
+};
+
 // add the shape to the layer
-layer.add(circle);
+layer.add(mainCircle);
+
+drawFriends();
 
 // add the shape to the layer
 //layer.add(rect);
