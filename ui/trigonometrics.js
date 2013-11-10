19642b0eeb452e9b94c5dd081ffb4d8628c8377f
schernyshov <Sergey_Chernyshov@epam.com>
2013-11-11 00:39:50 +0300

    Немного допилил анимацию

--- ui/trigonometrics.js
+++ ui/trigonometrics.js
@@ -1,46 +1,88 @@
-function getAngleToPoint(x, y, robot) {
-  var dx = x - robot.position.x;
-  var dy = y - robot.position.y;
-  var angle = Math.atan(dy/dx) * 180 / Math.PI;
-  var targetAngle = 0; 
-//  robot.log("\nx: " + x + "\ny: " + y + "\nrx: " + robot.position.x + "\nry: " + robot.position.y);
-  
-  if (dx > 0 && dy > 0) {
-//    robot.log("dx > 0 && dy > 0");
-		targetAngle = 90 + angle;		    
-  }
-  if (dx > 0 && dy < 0) {
-//    robot.log("dx > 0 && dy < 0");
-    targetAngle = 90 + angle;
-  }
-  if (dx < 0 && dy < 0) {
-//    robot.log("dx < 0 && dy < 0");
-    targetAngle = 270 + angle;
-  }
-  if (dx < 0 && dy > 0) {
-//    robot.log("dx < 0 && dy > 0");
-    targetAngle = 270 + angle;
-  }  
-  if (dx == 0) {
-		  if (dy > 0) {
-		  	targetAngle = 90;	  
-      } else if (dy > 0) {
-        targetAngle = 270;
-      } else {
-      	targetAngle = robot.angle; // change point  
-      }    
-  }  
-  if (dy == 0) {
-		  if (dx > 0) {
-		  	targetAngle = 0;  
-      } else if (dx > 0) {
-      	targetAngle = 180;  
-      } else {
-      	targetAngle = robot.angle; // change point  
-      }     
-  }
-  return Math.round(targetAngle);
+function getPointOnLine(start_point, end_point, distance) {
+  var dx = end_point.x - start_point.x;
+  var dy = end_point.y - start_point.y;
+  var fraction = Math.max(Math.abs(dx), Math.abs(dy)) / distance;
+  var x = start_point.x + dx / fraction;
+  var y = start_point.y + dy / fraction;
+  return {x:Math.round(x), y:Math.round(y)};    
 }
+
+// function getPointOnLine1(start_point, end_point, distance) {
+//   var angle = getAngleToPoint(start_point, end_point);
+//   var x = Math.sqrt(distance * distance / (1 + Math.tan(angle) * Math.tan(angle)));
+//   var y = Math.abs(x * Math.tan(angle));
+//   var dx = end_point.x - start_point.x;
+//   var dy = end_point.y - start_point.y;
+
+//   if (angle > 270) {
+//     x = start_point.x + x;
+//     y = start_point.y - y;
+//   } else if (angle > 180) {
+//     x = start_point.x - x;
+//     y = start_point.y - y;
+//   } else if (angle > 90) {
+//     x = start_point.x - x;
+//     y = start_point.y + y;
+//   } else {
+//     x = start_point.x + x;
+//     y = start_point.y + y;
+//   } 
+
+//   console.log(x);
+//   console.log(y);  
+//   return {x:Math.round(x), y:Math.round(y)};    
+// }
+
+// function getAngleToPoint(start_point, end_point) {
+//   var dx = end_point.x - start_point.x;
+//   var dy = end_point.y - start_point.y;
+//   var angle = Math.atan(dy/dx) * 180 / Math.PI;
+//   var targetAngle = 0; 
+// //  robot.log("\nx: " + x + "\ny: " + y + "\nrx: " + robot.position.x + "\nry: " + robot.position.y);
+  
+//   // I quater
+//   if (dx > 0 && dy > 0) {
+// 		targetAngle = angle;		    
+//   }
+//   // IV quater
+//   if (dx > 0 && dy < 0) {
+// //    robot.log("dx > 0 && dy < 0");
+//     targetAngle = 360 + angle;
+//   }
+//   // III quater
+//   if (dx < 0 && dy < 0) {
+// //    robot.log("dx < 0 && dy < 0");
+//     targetAngle = 180 + angle;
+//   }
+//   // II quater
+//   if (dx < 0 && dy > 0) {
+// //    robot.log("dx < 0 && dy > 0");
+//     targetAngle = 180 + angle;
+//   }  
+//   if (dx == 0) {
+// 		  if (dy > 0) {
+// 		  	targetAngle = 90;	  
+//       } else if (dy > 0) {
+//         targetAngle = 270;
+//       } else {
+//       	targetAngle = angle; // change point  
+//       }    
+//   }  
+//   if (dy == 0) {
+// 		  if (dx > 0) {
+// 		  	targetAngle = 0;  
+//       } else if (dx > 0) {
+//       	targetAngle = 180;  
+//       } else {
+//       	targetAngle = angle; // change point  
+//       }     
+//   }
+//   console.log(dx);
+//   console.log(dy);
+//   console.log(angle);
+//   console.log(targetAngle);
+//   return Math.round(targetAngle);
+// }
 
 // var volley = 0;
 // function fireVolley(robot) {
